80e8f1ce24df3016c18ee398899c4ebee0a35d8a
Rick Huang <142161800+ThatRickHuang@users.noreply.github.com>
2024-01-01 00:39:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+document.addEventListener("DOMContentLoaded", function () {
+    var menuIcon = document.getElementById("menu-icon");
+    var menuLinks = document.getElementById("menu-links");
+
+    menuIcon.addEventListener("click", function () {
+        menuLinks.classList.toggle("show");
+    });
+});
